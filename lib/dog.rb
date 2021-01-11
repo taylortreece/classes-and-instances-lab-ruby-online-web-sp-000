ac0ea09052b3f72dc05012ac2deea8c8cad61ef3
Taylor Treece <tayloratreece@gmail.com>
2021-01-11 18:52:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  puts "woof"
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  puts "woof"
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
